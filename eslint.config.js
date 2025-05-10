6204a85f8320227fb930a98023cd395f7c113cfd
suthikawong <suthika.wsrd@gmail.com>
2025-05-10 23:47:57 +0100

    use eslint-plugin-unicorn

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import eslintPluginUnicorn from 'eslint-plugin-unicorn'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  eslintPluginUnicorn.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'react-compiler': reactCompiler,
-    },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'react-compiler/react-compiler': 'error',
+      'unicorn/better-regex': 'warn',
     },
-  }
-)
+  },
+]
