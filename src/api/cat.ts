6bb6190223de57f5ec4efb1038ef5d6c3146fcd2
suthikawong <suthika.wsrd@gmail.com>
2025-05-11 18:07:27 +0100

    change dto path

--- src/api/cat.ts
+++ src/api/cat.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
-import { AppResponse } from '../../gpa-backend/dtos/app'
-import { GetCatResponse } from '../../gpa-backend/dtos/cats/cats-response.dto'
+import { AppResponse } from '../../gpa-backend/src/app.response'
+import { GetCatResponse } from '../../gpa-backend/src/cats/dto/cats.response'
 
 const getCats = async (): Promise<AppResponse<GetCatResponse>> => {
   const response = await axios.get(`${import.meta.env.VITE_API_URL}/cats`)
