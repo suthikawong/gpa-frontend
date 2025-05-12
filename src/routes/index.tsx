60578c2b72bee38faa2375817e40a86bca47161c
suthikawong <suthika.wsrd@gmail.com>
2025-05-12 02:02:49 +0100

    init shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 import { api } from '../api'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
@@ -9,14 +10,17 @@ export const Route = createFileRoute('/')({
 function RouteComponent() {
   const { data: res, isLoading, error } = useQuery({ queryKey: ['cats'], queryFn: api.cat.getCats })
 
-  if (isLoading) return <p>Loading...</p>
+  // if (isLoading) return <p>Loading...</p>
 
-  if (error) return <p>Error occured: {error.message}</p>
+  // if (error) return <p>Error occured: {error.message}</p>
 
   return (
     <>
       <div>
+        <Button>Login</Button>
         <div className="text-lg text-astronaut-900">hello</div>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>Error occured: {error.message}</p>}
         {res?.data?.map((cat, index) => <p key={index}>{cat.name}</p>)}
       </div>
     </>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
@@ -14,5 +15,10 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   base: '/',
 })
